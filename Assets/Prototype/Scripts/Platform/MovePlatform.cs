05f02fe1486f468b13ef49da7cc911c03a2537e3
AleksNose <aleks.nosinska@gmail.com>
2022-10-30 00:22:07 +0200

    Add all

---
+++ Assets/Prototype/Scripts/Platform/MovePlatform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public TypeMove Type { get; private set; }
+
+    public void PlatformMode(bool mode)
+    {
+        Catch(mode);
+    }
+
+    public void Move(Vector3 vector)
+    {
+        Debug.Log(vector);
+        gameObject.transform.position = new Vector3(vector.x, vector.y, 0f);
+        Catch(true);
+    }
+
+    public void Catch(bool isCatch)
+    {
+        var collider = GetComponent<Collider2D>();
+        
+        if (isCatch)
+        {
+            collider.isTrigger = true;
+            return;
+        }
+
+        collider.isTrigger = false;
+    }
+}
